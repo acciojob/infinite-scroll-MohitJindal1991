11b746dd0fed8efa3f6f8f0d4f94f76375d7d39a
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-26 18:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+const element=document.querySelector('#infi-list');
+
+let c=1;
+function getInformation(){
+	for(let i=0;i<10;i++){
+		const Li=document.createElement('li');
+		Li.innerHTML=`Item ${c}`;
+		c++;
+
+		element.appendChild(Li);
+	}
+}
+
+
+element.addEventListener('scroll',()=>{
+	console.log('event')
+	if(element.scrollTop + element.clientHeight >=element.scrollHeight){
+getInformation();
+	}
+});
+getInformation();
